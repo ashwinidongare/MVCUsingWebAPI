8af0013d9ab5bf4fc04fee3349b9bec1edb9f385
ashwinidongare <ashwini.dongare@v2solutions.com>
2017-07-11 18:35:11 +0530

    Initial commit

---
+++ MVCUsingWebAPI/Controllers/SubcribersController.cs
@@ -0,0 +1,24 @@
+﻿using MVCUsingWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCUsingWebAPI.Controllers
+{
+    public class SubcribersController : ApiController
+    {
+
+        DataEntitiesContext dbContext = new DataEntitiesContext();
+        // GET api/values
+     public IEnumerable<Employee> Get()
+        {
+            return dbContext.Employees.ToList();
+            //return new string[] { "value1", "value2" };
+        }
+
+        
+    }
+}
---
+++ MVCUsingWebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCUsingWebAPI.WebApiApplication" Language="C#" %>
